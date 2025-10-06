ce25c94284fa09b3c438bd39675b3fcd31f84003
Emmanuel Cousin <emmanuel@hey.com>
2025-10-06 17:22:40 -0300

    eslint - avoid ambiguous props

--- app/javascript/pages/Admin/Search/Purchases/Index.tsx
+++ app/javascript/pages/Admin/Search/Purchases/Index.tsx
@@ -1,13 +1,31 @@
 import { usePage } from "@inertiajs/react";
 import React from "react";
 
-import AdminPurchases, { type PageProps } from "$app/components/Admin/Purchases";
+import { type Pagination as PaginationProps } from "$app/hooks/useLazyFetch";
+
+import AdminPurchases from "$app/components/Admin/Purchases";
+import { type Purchase } from "$app/components/Admin/Purchases/PurchaseDetails";
+
+type PageProps = {
+  purchases: Purchase[];
+  query: string;
+  product_title_query: string;
+  purchase_status: string;
+  pagination: PaginationProps;
+};
 
 const AdminSearchPurchases = () => {
-  const props = usePage().props as unknown as PageProps;
+  const { purchases, query, product_title_query, purchase_status, pagination } = usePage<PageProps>().props;
   return (
     <div className="space-y-4">
-      <AdminPurchases {...props} endpoint={Routes.admin_search_purchases_path} />
+      <AdminPurchases
+        purchases={purchases}
+        query={query}
+        product_title_query={product_title_query}
+        purchase_status={purchase_status}
+        pagination={pagination}
+        endpoint={Routes.admin_search_purchases_path}
+      />
     </div>
   );
 };
